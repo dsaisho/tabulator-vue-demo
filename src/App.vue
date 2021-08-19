ac4acb8014f7b5ca213772a19a01c8c79318fe3b
Douglas Saisho <dsaisho@magnaflow.com>
2021-08-19 15:44:41 -0700

    basic vue tabulator demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="example-table"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Tabulator from 'tabulator-tables';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      table: null,
+      tableData: [
+      {
+        id: 1,
+        name: "Billy Bob",
+        age: "12",
+        gender: "male",
+        height: 1,
+        col: "red",
+        dob: "",
+        cheese: 1,
+      },
+      {
+        id: 2,
+        name: "Mary May",
+        age: "1",
+        gender: "female",
+        height: 2,
+        col: "blue",
+        dob: "14/05/1982",
+        cheese: true,
+      },
+    ]};
+  },
+  mounted() {
+
+    this.table = new Tabulator("#example-table", {
+      data: this.tableData, //set initial table data
+      columns: [
+        { title: "Name", field: "name" },
+        { title: "Age", field: "age" },
+        { title: "Gender", field: "gender" },
+        { title: "Height", field: "height" },
+        { title: "Favourite Color", field: "col" },
+        { title: "Date Of Birth", field: "dob" },
+        { title: "Cheese Preference", field: "cheese" },
+      ],
+    });
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+@import  "~vue-tabulator/dist/scss/bootstrap/tabulator_bootstrap4";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
